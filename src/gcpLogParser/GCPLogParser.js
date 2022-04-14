b29a3991733b9b5814ac3acb5547fc5b0f79ce6d
Kenny <kenny_hung@mail.chungyo.net>
2022-04-14 11:42:39 +0800

    轉換「轉帳成功截圖 base64」為圖片

--- src/gcpLogParser/GCPLogParser.js
+++ src/gcpLogParser/GCPLogParser.js
@@ -215,6 +215,20 @@ class GCPLogParser {
                 return;
             }
 
+            if (contentText.includes('轉帳成功截圖')) {
+                const base64 = contentText.match(/轉帳成功截圖： b'(?<base64>.+)'/).groups.base64;
+                const base64Image = `data:image/png;base64,${base64}`;
+                const image = document.createElement('img');
+                image.src = base64Image;
+                contentDom.innerHTML = '';
+                contentDom.append(image);
+                contentDom.onclick = () => {
+                    const newWindow = window.open();
+                    newWindow.document.write(image.outerHTML);
+                };
+                return;
+            }
+
             if (this.isJson(contentText)) {
                 const pre = this.generatePreDom(JSON.parse(contentText));
                 contentDom.innerHTML = '';
