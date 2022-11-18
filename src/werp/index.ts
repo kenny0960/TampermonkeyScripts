c703262da26a81052b56ba17f1c26116ac80935a
Kenny <kenny_hung@mail.chungyo.net>
2022-11-18 16:05:19 +0800

    更新 WREP V2.4.3
    - 更新特休 TOKEN

--- src/werp/index.ts
+++ src/werp/index.ts
@@ -337,6 +337,7 @@ const getAnnualLeaveTemplate = (annualLeave: AnnualLeave): string => {
 
 const getUpdateLogs = (): string[] => {
     return [
+        'v2.4.3(20221118) 更新特休 TOKEN',
         'v2.4.2(20221111) 更新特休 TOKEN',
         'v2.4.1(20221111) 修正 favicon 無限增生的問題',
         'v2.4.0(20221107) 修正 favicon 失效的問題',
