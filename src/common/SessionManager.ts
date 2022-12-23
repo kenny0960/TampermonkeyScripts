94cdf4a7fa94acd91d71563b11f70e8ea9d3554c
Kenny <kenny_hung@mail.chungyo.net>
2022-12-23 11:34:18 +0800

    新增 SessionManager getObjectByKey 函式

--- src/common/SessionManager.ts
+++ src/common/SessionManager.ts
@@ -1,3 +1,5 @@
+import { isJson } from '@/common/json';
+
 class SessionManager {
     static has(key: string): boolean {
         return sessionStorage.getItem(key) !== null;
@@ -8,6 +10,17 @@ class SessionManager {
             return '';
         }
         return sessionStorage.getItem(key);
+    }
+
+    static getObjectByKey(key: string): Object {
+        if (SessionManager.has(key) === false) {
+            return {};
+        }
+        const jsonString: string = sessionStorage.getItem(key);
+        if (isJson(jsonString) === true) {
+            return JSON.parse(jsonString);
+        }
+        return {};
     }
 
     static setByKey(key: string, value: string): void {
