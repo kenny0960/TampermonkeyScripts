0d8b5ca5e3f939b1d3197db61510925c9c21c8cc
Kenny <kenny_hung@mail.chungyo.net>
2022-12-26 14:04:28 +0800

    新增 LeaveNote 介面和預設值

---
+++ src/werp/classes/leaveNote.ts
@@ -0,0 +1,7 @@
+import LeaveNote from '@/werp/interfaces/LeaveNote';
+
+export const defaultLeaveNote: LeaveNote = {
+    unusualNote: '',
+    unsignedNote: '',
+    receiptNote: '',
+};
---
+++ src/werp/interfaces/LeaveNote.ts
@@ -0,0 +1,7 @@
+interface LeaveNote {
+    unusualNote: string;
+    unsignedNote: string;
+    receiptNote: string;
+}
+
+export default LeaveNote;
