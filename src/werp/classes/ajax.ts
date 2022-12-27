a98a2c5edceb75d177691aa053b4292b695dfc06
Kenny <kenny_hung@mail.chungyo.net>
2022-12-27 17:11:56 +0800

    新增取得請假狀況的 ajax

--- src/werp/classes/ajax.ts
+++ src/werp/classes/ajax.ts
@@ -1,6 +1,8 @@
+import * as moment from 'moment';
 import AnnualLeave from '@/werp/interfaces/AnnualLeave';
 import Attendance from '@/werp/interfaces/Attendance';
 import LeaveNote from '@/werp/interfaces/LeaveNote';
+import LeaveReceiptNote from '@/werp/interfaces/LeaveReceiptNote';
 
 export const fetchCompanyEmployeeToken = async (): Promise<string | null> => {
     return await fetch('https://cy.iwerp.net/system/hr/showEmpData.xhtml', {
@@ -276,5 +278,113 @@ export const fetchPersonalLeaveNotes = async (firstDayAttendance: Attendance): P
                 }
             );
             return leaveNotes;
+        });
+};
+
+export const fetchPersonalLeaveReceiptNotesSearchPattern = async (): Promise<string[]> => {
+    return await fetch('https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml', {
+        headers: {
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6,zh-CN;q=0.5',
+            'cache-control': 'no-cache',
+            pragma: 'no-cache',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+        },
+        referrer: 'https://cy.iwerp.net/portal/page/new_home.xhtml',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+    })
+        .then((response) => {
+            return response.text();
+        })
+        .then((body) => {
+            const html: HTMLHtmlElement = document.createElement('html');
+            html.innerHTML = body;
+            const inputElements: NodeListOf<HTMLInputElement> = html.querySelectorAll('#searchContent input');
+            if (inputElements.length < 2) {
+                return [];
+            }
+            return [inputElements.item(0).name, inputElements.item(1).name];
+        });
+};
+
+export const fetchPersonalLeaveReceiptNotes = async (): Promise<LeaveReceiptNote[]> => {
+    const searchPattern: string[] = await fetchPersonalLeaveReceiptNotesSearchPattern();
+    const endDate: string = moment().day(28).format('YYYY/MM/DD', { trim: false });
+    const startDate: string = moment().day(-7).format('YYYY/MM/DD', { trim: false });
+    const searchDateRange: string = `&${searchPattern[0]}=${startDate}&${searchPattern[1]}=${endDate}`;
+
+    return fetch('https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml', {
+        headers: {
+            accept: 'application/xml, text/xml, */*; q=0.01',
+            'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6,zh-CN;q=0.5',
+            'cache-control': 'no-cache',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'faces-request': 'partial/ajax',
+            pragma: 'no-cache',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'x-requested-with': 'XMLHttpRequest',
+        },
+        referrer: 'https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: `javax.faces.partial.ajax=true&javax.faces.source=search-btn&javax.faces.partial.execute=search-btn+searchContent&javax.faces.partial.render=search-btn+id_tag_toolbar_view+leaveaply-datatable&search-btn=search-btn&formTemplate=formTemplate&j_idt154_input=2022%2F11%2F26&j_idt158_input=2022%2F12%2F25&j_idt166_focus=&j_idt166_input=&j_idt170_focus=&j_idt170_input=&j_idt179_focus=&j_idt179_input=0&aply-data-tb%3Aj_idt193%3Afilter=&aply-data-tb%3Aj_idt195%3Afilter=&aply-data-tb%3Aj_idt197%3Afilter=&aply-data-tb%3Aj_idt199%3Afilter=&aply-data-tb%3Aj_idt202%3Afilter=&aply-data-tb%3Aj_idt204%3Afilter=&aply-data-tb%3Aj_idt206%3Afilter=&aply-data-tb%3Aj_idt208%3Afilter=&aply-data-tb%3Aj_idt210%3Afilter=&aply-data-tb%3Aj_idt215_focus=&aply-data-tb%3Aj_idt215_input=&aply-data-tb%3Aj_idt219%3Afilter=&aply-data-tb%3Aj_idt222_focus=&aply-data-tb%3Aj_idt222_input=&aply-data-tb%3Aj_idt227_focus=&aply-data-tb%3Aj_idt227_input=&aply-data-tb%3Aj_idt232_focus=&aply-data-tb%3Aj_idt232_input=&aply-data-tb_selection=&aply-data-tb_scrollState=0%2C0&j_idt672%3Adlg-leave-aply-vac-table%3Aj_idt676%3Afilter=&j_idt672%3Adlg-leave-aply-vac-table_selection=&j_idt672%3Adlg-leave-aply-vac-table_scrollState=0%2C0&j_idt729=&j_idt734%3Aj_idt744=&javax.faces.ViewState=${await fetchAnnualLeaveToken()}`.replace(
+            /&j_idt154_input=\d+%2F\d+%2F\d+&j_idt158_input=\d+%2F\d+%2F\d+/,
+            searchDateRange
+        ),
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+    })
+        .then((response) => {
+            return response.text();
+        })
+        .then((body) => {
+            const leaveReceiptNotes: LeaveReceiptNote[] = [];
+            const html: HTMLHtmlElement = document.createElement('html');
+            html.innerHTML = body;
+            html.querySelectorAll('.ui-datatable-scrollable-body tbody tr:not(.ui-datatable-empty-message)').forEach(
+                (tr: HTMLTableRowElement) => {
+                    const tds: HTMLCollectionOf<HTMLTableCellElement> = tr.getElementsByTagName('td');
+
+                    if (tds.length === 0) {
+                        return;
+                    }
+
+                    const typeElement: HTMLDivElement | null = tds.item(6);
+                    const noteElement: HTMLDivElement | null = tds.item(7);
+                    const startDateElement: HTMLDivElement | null = tds.item(8);
+                    const startTimeElement: HTMLDivElement | null = tds.item(9);
+                    const endDateElement: HTMLDivElement | null = tds.item(10);
+                    const endTimeElement: HTMLDivElement | null = tds.item(11);
+                    const hoursElement: HTMLDivElement | null = tds.item(12);
+                    const statusElement: HTMLDivElement | null = tds.item(15);
+                    if (typeElement !== null && statusElement !== null) {
+                        leaveReceiptNotes.push({
+                            type: typeElement.innerText.trim(),
+                            note: noteElement.innerText.trim(),
+                            start: moment(`${startDateElement.innerText.trim()} ${startTimeElement.innerText.trim()}`),
+                            end: moment(`${endDateElement.innerText.trim()} ${endTimeElement.innerText.trim()}`),
+                            hours: Number(hoursElement.innerText.trim()),
+                            status: statusElement.innerText.trim(),
+                        });
+                    }
+                }
+            );
+            return leaveReceiptNotes;
         });
 };
