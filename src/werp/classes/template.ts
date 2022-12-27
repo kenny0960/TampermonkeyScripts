fbd49a0506054dd7249b141a2eb830b49ad78dc2
Kenny <kenny_hung@mail.chungyo.net>
2022-12-27 18:07:29 +0800

    新增近期請假狀況的模板

--- src/werp/classes/template.ts
+++ src/werp/classes/template.ts
@@ -3,6 +3,7 @@ import { formatTime, formatWeekday, isToday } from '@/werp/classes/momentUtility
 import AnnualLeave from '@/werp/interfaces/AnnualLeave';
 import LeaveNote from '@/werp/interfaces/LeaveNote';
 import * as moment from 'moment';
+import LeaveReceiptNote from '@/werp/interfaces/LeaveReceiptNote';
 
 export const getAnnualLeaveTemplate = (annualLeave: AnnualLeave | null): string => {
     if (annualLeave === null) {
@@ -32,6 +33,88 @@ export const getAnnualLeaveTemplate = (annualLeave: AnnualLeave | null): string 
             有效日：${annualLeave.endDatetime}
           </li>
         </ul>
+      </span>
+    </div>
+  </div>
+</div>
+<table id="formTemplate:j_idt319" class="ui-panelgrid ui-widget" style=" width: 100%; border: none;margin-top: 2px;margin-bottom: 2px; " role="grid"><tbody><tr class="ui-widget-content ui-panelgrid-even" role="row"><td role="gridcell" class="ui-panelgrid-cell" style="border-bottom-color: #C4C4C4;border-bottom-width: 0.5px;border-top-color: white;                                border-left-color: white;border-right-color: white;"></td></tr></tbody></table>
+    `;
+};
+
+export const getLeaveReceiptNotesTemplate = (leaveReceiptNotes: LeaveReceiptNote[]): string => {
+    if (leaveReceiptNotes.length === 0) {
+        return '';
+    }
+    const templates: string[] = [];
+    for (const leaveReceiptNote of leaveReceiptNotes.reverse()) {
+        // 不顯示已結案的過往記錄
+        if (leaveReceiptNote.status === '結案' && leaveReceiptNote.start.isBefore(moment()) === true) {
+            continue;
+        }
+        templates.push(`
+            <tr data-ri="0" style="${
+                leaveReceiptNote.start.isBefore(moment()) ? 'opacity: 0.5;' : ''
+            }" class="ui-widget-content ui-datatable-even" role="row">
+                <td role="gridcell" style="text-align: center;" class="notSign">
+                    ${
+                        leaveReceiptNote.status === '結案'
+                            ? '<i style="color: darkgreen;" class="fa fa-1 fa-check-square-o"></i>'
+                            : '<i style="color: chocolate;" class="fa fa-spinner fa-1 fa-spin fa-fw"></i>'
+                    }
+                </td>
+                <td role="gridcell" style="text-align: center;" class="notSign">
+                    ${leaveReceiptNote.type}
+                </td>
+                <td role="gridcell" style="text-align: center; font-size: 12px;">
+                    ${leaveReceiptNote.start.format('YYYY/MM/DD (dd) HH:mm', { trim: false })}
+                    ${leaveReceiptNote.end.format('YYYY/MM/DD (dd) HH:mm', { trim: false })}
+                </td>
+                <td role="gridcell" style="text-align: center;" class="notSign">
+                    ${leaveReceiptNote.hours}
+                </td>
+                <td role="gridcell" style="text-align: center;" class="notSign">
+                    ${leaveReceiptNote.status}
+                </td>
+                 <td role="gridcell" style="text-align: center;" class="notSign">
+                    ${leaveReceiptNote.start.fromNow()}
+                </td>
+            </tr>
+        `);
+    }
+
+    return `
+<div id="formTemplate:j_idt323" class="ui-outputpanel ui-widget">
+  <div class="ui-g-12 waiting-task-g">
+    <div class="title-name ui-g-4">近期請假狀況</div>
+    <div class="ui-g-8">
+      <span class="todocss">
+        <table role="grid" style="width: 100%;">
+          <thead id="formTemplate:anno-datatable_head">
+            <tr role="row" style="border-bottom: 1px solid darkgray;">
+            <th id="formTemplate:anno-datatable:j_idt707" role="columnheader" aria-label="" scope="col" style="text-align: center;width:8px">
+                <span class="ui-column-title" style="margin-right: 2px;"></span>
+              </th>
+              <th id="formTemplate:anno-datatable:j_idt707" role="columnheader" aria-label="假別" scope="col" style="text-align: center;">
+                <span class="ui-column-title" style="margin-right: 2px;">假別</span>
+              </th>
+              <th id="formTemplate:anno-datatable:j_idt710" role="columnheader" scope="col" style="text-align: center;width:130px">
+                <span class="ui-column-title" style="margin-right: 2px;">時間</span>
+              </th>
+              <th id="formTemplate:anno-datatable:j_idt713" role="columnheader" aria-label="小時" scope="col" style="text-align: center;width:35px">
+                <span class="ui-column-title" style="margin-right: 2px;">小時</span>
+              </th>
+              <th id="formTemplate:anno-datatable:j_idt715" role="columnheader" aria-label="狀態" scope="col" style="text-align: center;">
+                <span class="ui-column-title" style="margin-right: 2px;">狀態</span>
+              </th>
+              <th id="formTemplate:anno-datatable:j_idt715" role="columnheader" aria-label="" scope="col" style="text-align: center;">
+                <span class="ui-column-title" style="margin-right: 2px;"></span>
+              </th>
+            </tr>
+          </thead>
+          <tbody id="formTemplate:anno-datatable_data" class="ui-datatable-data ui-widget-content">
+            ${templates.join('')}
+          </tbody>
+        </table>
       </span>
     </div>
   </div>
