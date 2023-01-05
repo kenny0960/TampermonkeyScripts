f5b59c43708637967106a1389e79ed5ad861e609
Kenny <kenny_hung@mail.chungyo.net>
2023-01-05 10:35:33 +0800

    簡化更新簽到退資訊的定時器寫法

--- src/werp/classes/timer.ts
+++ src/werp/classes/timer.ts
@@ -8,9 +8,7 @@ import { log } from '@/common/logger';
 import { updateTodayAttendanceContent } from '@/werp';
 
 export const resetAttendanceTimers = (): void => {
-    window.clearTimeout(Number(SessionManager.getByKey(SessionKeys.SIGN_IN_NOTIFICATION_TIMER)));
-    window.clearTimeout(Number(SessionManager.getByKey(SessionKeys.TODAY_ATTENDANCE_CONTENT_TIMER)));
-    window.clearTimeout(Number(SessionManager.getByKey(SessionKeys.TODAY_ATTENDANCE_FAVICON_TIMER)));
+    window.clearTimeout(Number(SessionManager.getByKey(SessionKeys.ATTENDANCE_TIMER)));
 };
 
 export const startAttendanceTimers = (table: HTMLTableElement, attendances: Attendance[]) => {
@@ -21,25 +19,11 @@ export const startAttendanceTimers = (table: HTMLTableElement, attendances: Atte
             showSignInNotification(attendances);
 
             // 定時更新內容
-            const todayAttendanceContentTimer: number = window.setTimeout((): void => {
-                log('更新預設當日下班內容');
-                updateTodayAttendanceContent(table, attendances);
+            const attendanceTimer: number = window.setTimeout((): void => {
+                log('更新資訊');
+                startAttendanceTimers(table, attendances);
             }, 60 * 1000);
-            SessionManager.setByKey(SessionKeys.TODAY_ATTENDANCE_CONTENT_TIMER, String(todayAttendanceContentTimer));
-
-            // 定時更新內容
-            const todayAttendanceFaviconTimer: number = window.setTimeout((): void => {
-                log('更新預設當日下班 Favicon');
-                updateAttendanceFavicon(attendances);
-            }, 60 * 1000);
-            SessionManager.setByKey(SessionKeys.TODAY_ATTENDANCE_FAVICON_TIMER, String(todayAttendanceFaviconTimer));
-
-            // 定時更新內容
-            const signInNotificationTimer: number = window.setTimeout(
-                (): void => showSignInNotification(attendances),
-                5 * 60 * 1000
-            );
-            SessionManager.setByKey(SessionKeys.SIGN_IN_NOTIFICATION_TIMER, String(signInNotificationTimer));
+            SessionManager.setByKey(SessionKeys.ATTENDANCE_TIMER, String(attendanceTimer));
         }
     }
 };
