33974eff0a6e482aa7779d42a02f3fd3dc00374d
Kenny <kenny_hung@mail.chungyo.net>
2023-01-10 14:54:47 +0800

    新增 ProgressBar 介面和初始值

---
+++ src/werp/classes/progressBar.ts
@@ -0,0 +1,7 @@
+import ProgressBar from '@/werp/interfaces/ProgressBar';
+
+export const defaultProgressBar: ProgressBar = {
+    percentage: 0,
+    text: '',
+    textClass: '',
+};
---
+++ src/werp/interfaces/ProgressBar.ts
@@ -0,0 +1,7 @@
+interface ProgressBar {
+    percentage: number;
+    text: string;
+    textClass: string;
+}
+
+export default ProgressBar;
