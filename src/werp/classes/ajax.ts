3ea8aa77f8e99a96c02e65bfbf177e78d0bea843
Kenny <kenny_hung@mail.chungyo.net>
2023-02-08 11:26:20 +0800

    透過 AjaxPattern 來減少取得 Ajax 的次數

--- src/werp/classes/ajax.ts
+++ src/werp/classes/ajax.ts
@@ -4,6 +4,7 @@ import { Moment } from '@/moment';
 import LeaveNote from '@/werp/interfaces/LeaveNote';
 import LeaveReceiptNote from '@/werp/interfaces/LeaveReceiptNote';
 import { getPickedDate } from '@/werp/classes/momentUtility';
+import AjaxPattern from '@/werp/interfaces/AjaxPattern';
 
 export const fetchCompanyEmployeeToken = async (): Promise<string | null> => {
     return await fetch('https://cy.iwerp.net/system/hr/showEmpData.xhtml', {
@@ -77,43 +78,8 @@ export const fetchAllCompanyEmployeeCount = async (): Promise<number | null> => 
         });
 };
 
-export const fetchAnnualLeaveToken = async (): Promise<string | null> => {
-    return await fetch('https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml', {
-        headers: {
-            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6,zh-CN;q=0.5',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-        },
-        referrer: 'https://cy.iwerp.net/portal/page/new_home.xhtml',
-        referrerPolicy: 'strict-origin-when-cross-origin',
-        body: null,
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-    })
-        .then((response) => {
-            return response.text();
-        })
-        .then((body) => {
-            const html: HTMLHtmlElement = document.createElement('html');
-            html.innerHTML = body;
-            const inputElement: HTMLInputElement | null = html.querySelector('input[id="j_id1:javax.faces.ViewState:0"]');
-            if (inputElement === null) {
-                return null;
-            }
-            return encodeURIComponent(inputElement.value.trim());
-        });
-};
-
 export const fetchAnnualLeave = async (): Promise<AnnualLeave | null> => {
+    const { session }: AjaxPattern = await fetchPersonalLeaveReceiptNotesAjaxPattern();
     return await fetch('https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml', {
         headers: {
             accept: 'application/xml, text/xml, */*; q=0.01',
@@ -130,7 +96,7 @@ export const fetchAnnualLeave = async (): Promise<AnnualLeave | null> => {
         },
         referrer: 'https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml',
         referrerPolicy: 'strict-origin-when-cross-origin',
-        body: `javax.faces.partial.ajax=true&javax.faces.source=new-btn&javax.faces.partial.execute=new-btn&javax.faces.partial.render=new-btn+id_tag_toolbar_view+containter&new-btn=new-btn&formTemplate=formTemplate&j_idt154_input=2022%2F11%2F26&j_idt158_input=2022%2F12%2F25&j_idt166_focus=&j_idt166_input=&j_idt170_focus=&j_idt170_input=&j_idt179_focus=&j_idt179_input=0&aply-data-tb%3Aj_idt193%3Afilter=&aply-data-tb%3Aj_idt195%3Afilter=&aply-data-tb%3Aj_idt197%3Afilter=&aply-data-tb%3Aj_idt199%3Afilter=&aply-data-tb%3Aj_idt202%3Afilter=&aply-data-tb%3Aj_idt204%3Afilter=&aply-data-tb%3Aj_idt206%3Afilter=&aply-data-tb%3Aj_idt208%3Afilter=&aply-data-tb%3Aj_idt210%3Afilter=&aply-data-tb%3Aj_idt215_focus=&aply-data-tb%3Aj_idt215_input=&aply-data-tb%3Aj_idt219%3Afilter=&aply-data-tb%3Aj_idt222_focus=&aply-data-tb%3Aj_idt222_input=&aply-data-tb%3Aj_idt227_focus=&aply-data-tb%3Aj_idt227_input=&aply-data-tb%3Aj_idt232_focus=&aply-data-tb%3Aj_idt232_input=&aply-data-tb_selection=&aply-data-tb_scrollState=0%2C0&j_idt672%3Adlg-leave-aply-vac-table%3Aj_idt676%3Afilter=&j_idt672%3Adlg-leave-aply-vac-table_selection=&j_idt672%3Adlg-leave-aply-vac-table_scrollState=0%2C0&j_idt729=&j_idt734%3Aj_idt744=&javax.faces.ViewState=${await fetchAnnualLeaveToken()}`,
+        body: `javax.faces.partial.ajax=true&javax.faces.source=new-btn&javax.faces.partial.execute=new-btn&javax.faces.partial.render=new-btn+id_tag_toolbar_view+containter&new-btn=new-btn&formTemplate=formTemplate&j_idt154_input=2022%2F11%2F26&j_idt158_input=2022%2F12%2F25&j_idt166_focus=&j_idt166_input=&j_idt170_focus=&j_idt170_input=&j_idt179_focus=&j_idt179_input=0&aply-data-tb%3Aj_idt193%3Afilter=&aply-data-tb%3Aj_idt195%3Afilter=&aply-data-tb%3Aj_idt197%3Afilter=&aply-data-tb%3Aj_idt199%3Afilter=&aply-data-tb%3Aj_idt202%3Afilter=&aply-data-tb%3Aj_idt204%3Afilter=&aply-data-tb%3Aj_idt206%3Afilter=&aply-data-tb%3Aj_idt208%3Afilter=&aply-data-tb%3Aj_idt210%3Afilter=&aply-data-tb%3Aj_idt215_focus=&aply-data-tb%3Aj_idt215_input=&aply-data-tb%3Aj_idt219%3Afilter=&aply-data-tb%3Aj_idt222_focus=&aply-data-tb%3Aj_idt222_input=&aply-data-tb%3Aj_idt227_focus=&aply-data-tb%3Aj_idt227_input=&aply-data-tb%3Aj_idt232_focus=&aply-data-tb%3Aj_idt232_input=&aply-data-tb_selection=&aply-data-tb_scrollState=0%2C0&j_idt672%3Adlg-leave-aply-vac-table%3Aj_idt676%3Afilter=&j_idt672%3Adlg-leave-aply-vac-table_selection=&j_idt672%3Adlg-leave-aply-vac-table_scrollState=0%2C0&j_idt729=&j_idt734%3Aj_idt744=&javax.faces.ViewState=${session}`,
         method: 'POST',
         mode: 'cors',
         credentials: 'include',
@@ -155,7 +121,7 @@ export const fetchAnnualLeave = async (): Promise<AnnualLeave | null> => {
         });
 };
 
-export const fetchPersonalLeaveNotesToken = async (): Promise<string | null> => {
+export const fetchPersonalLeaveNotesAjaxPattern = async (): Promise<AjaxPattern | null> => {
     return await fetch('https://cy.iwerp.net/hr-attendance/merge/personal.xhtml', {
         headers: {
             accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -183,56 +149,27 @@ export const fetchPersonalLeaveNotesToken = async (): Promise<string | null> => 
         .then((body) => {
             const html: HTMLHtmlElement = document.createElement('html');
             html.innerHTML = body;
-            const inputElement: HTMLInputElement | null = html.querySelector('input[id="j_id1:javax.faces.ViewState:0"]');
-            if (inputElement === null) {
+            const sessionElement: HTMLInputElement | null = html.querySelector('input[id="j_id1:javax.faces.ViewState:0"]');
+            const searchElements: NodeListOf<HTMLInputElement> = html.querySelectorAll('#searchContent input');
+            if (sessionElement === null || searchElements.length < 2) {
                 return null;
             }
-            return encodeURIComponent(inputElement.value.trim());
-        });
-};
-
-export const fetchPersonalLeaveNotesSearchPattern = async (): Promise<string[]> => {
-    return await fetch('https://cy.iwerp.net/hr-attendance/merge/personal.xhtml', {
-        headers: {
-            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,vi;q=0.6,zh-CN;q=0.5',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-        },
-        referrer: 'https://cy.iwerp.net/portal/page/new_home.xhtml',
-        referrerPolicy: 'strict-origin-when-cross-origin',
-        body: null,
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-    })
-        .then((response) => {
-            return response.text();
-        })
-        .then((body) => {
-            const html: HTMLHtmlElement = document.createElement('html');
-            html.innerHTML = body;
-            const inputElements: NodeListOf<HTMLInputElement> = html.querySelectorAll('#searchContent input');
-            if (inputElements.length < 2) {
-                return [];
-            }
-            return [inputElements.item(0).name, inputElements.item(1).name];
+            return {
+                session: encodeURIComponent(sessionElement.value.trim()),
+                searchRange: {
+                    start: searchElements.item(0).name,
+                    end: searchElements.item(1).name,
+                },
+            };
         });
 };
 
 export const fetchPersonalLeaveNotes = async (): Promise<LeaveNote[]> => {
     const pickedDate: Moment = getPickedDate();
-    const searchPattern: string[] = await fetchPersonalLeaveNotesSearchPattern();
+    const { searchRange, session }: AjaxPattern = await fetchPersonalLeaveNotesAjaxPattern();
     const endDate: string = pickedDate.day(5).format('YYYY/MM/DD', { trim: false });
     const startDate: string = pickedDate.day(1).format('YYYY/MM/DD', { trim: false });
-    const searchDateRange: string = `&${searchPattern[0]}=${startDate}&${searchPattern[1]}=${endDate}`;
+    const searchDateRange: string = `&${searchRange.start}=${startDate}&${searchRange.end}=${endDate}`;
 
     return fetch('https://cy.iwerp.net/hr-attendance/merge/personal.xhtml', {
         headers: {
@@ -250,7 +187,7 @@ export const fetchPersonalLeaveNotes = async (): Promise<LeaveNote[]> => {
         },
         referrer: 'https://cy.iwerp.net/hr-attendance/merge/personal.xhtml',
         referrerPolicy: 'strict-origin-when-cross-origin',
-        body: `javax.faces.partial.ajax=true&javax.faces.source=search-btn&javax.faces.partial.execute=search-btn+searchContent&javax.faces.partial.render=search-btn+id_tag_toolbar_view+opDt&search-btn=search-btn&formTemplate=formTemplate&j_idt152_input=2022%2F11%2F26&j_idt156_input=2022%2F12%2F25&kind_focus=&kind_input=ALL&dt_scrollState=0%2C0&javax.faces.ViewState=${await fetchPersonalLeaveNotesToken()}`.replace(
+        body: `javax.faces.partial.ajax=true&javax.faces.source=search-btn&javax.faces.partial.execute=search-btn+searchContent&javax.faces.partial.render=search-btn+id_tag_toolbar_view+opDt&search-btn=search-btn&formTemplate=formTemplate&j_idt152_input=2022%2F11%2F26&j_idt156_input=2022%2F12%2F25&kind_focus=&kind_input=ALL&dt_scrollState=0%2C0&javax.faces.ViewState=${session}`.replace(
             /&j_idt152_input=\d+%2F\d+%2F\d+&j_idt156_input=\d+%2F\d+%2F\d+/,
             searchDateRange
         ),
@@ -283,7 +220,7 @@ export const fetchPersonalLeaveNotes = async (): Promise<LeaveNote[]> => {
         });
 };
 
-export const fetchPersonalLeaveReceiptNotesSearchPattern = async (): Promise<string[]> => {
+export const fetchPersonalLeaveReceiptNotesAjaxPattern = async (): Promise<AjaxPattern> => {
     return await fetch('https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml', {
         headers: {
             accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -312,19 +249,26 @@ export const fetchPersonalLeaveReceiptNotesSearchPattern = async (): Promise<str
         .then((body) => {
             const html: HTMLHtmlElement = document.createElement('html');
             html.innerHTML = body;
-            const inputElements: NodeListOf<HTMLInputElement> = html.querySelectorAll('#searchContent input');
-            if (inputElements.length < 2) {
-                return [];
+            const sessionElement: HTMLInputElement | null = html.querySelector('input[id="j_id1:javax.faces.ViewState:0"]');
+            const searchElements: NodeListOf<HTMLInputElement> = html.querySelectorAll('#searchContent input');
+            if (sessionElement === null || searchElements.length < 2) {
+                return null;
             }
-            return [inputElements.item(0).name, inputElements.item(1).name];
+            return {
+                session: encodeURIComponent(sessionElement.value.trim()),
+                searchRange: {
+                    start: searchElements.item(0).name,
+                    end: searchElements.item(1).name,
+                },
+            };
         });
 };
 
 export const fetchPersonalLeaveReceiptNotes = async (): Promise<LeaveReceiptNote[]> => {
-    const searchPattern: string[] = await fetchPersonalLeaveReceiptNotesSearchPattern();
+    const { session, searchRange }: AjaxPattern = await fetchPersonalLeaveReceiptNotesAjaxPattern();
     const endDate: string = moment().add(1, 'months').format('YYYY/MM/DD', { trim: false });
     const startDate: string = moment().subtract(7, 'days').format('YYYY/MM/DD', { trim: false });
-    const searchDateRange: string = `&${searchPattern[0]}=${startDate}&${searchPattern[1]}=${endDate}`;
+    const searchDateRange: string = `&${searchRange.start}=${startDate}&${searchRange.end}=${endDate}`;
 
     return fetch('https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml', {
         headers: {
@@ -344,7 +288,7 @@ export const fetchPersonalLeaveReceiptNotes = async (): Promise<LeaveReceiptNote
         },
         referrer: 'https://cy.iwerp.net/hr-attendance/leave/personal/personal-apply.xhtml',
         referrerPolicy: 'strict-origin-when-cross-origin',
-        body: `javax.faces.partial.ajax=true&javax.faces.source=search-btn&javax.faces.partial.execute=search-btn+searchContent&javax.faces.partial.render=search-btn+id_tag_toolbar_view+leaveaply-datatable&search-btn=search-btn&formTemplate=formTemplate&j_idt154_input=2022%2F11%2F26&j_idt158_input=2022%2F12%2F25&j_idt166_focus=&j_idt166_input=&j_idt170_focus=&j_idt170_input=&j_idt179_focus=&j_idt179_input=0&aply-data-tb%3Aj_idt193%3Afilter=&aply-data-tb%3Aj_idt195%3Afilter=&aply-data-tb%3Aj_idt197%3Afilter=&aply-data-tb%3Aj_idt199%3Afilter=&aply-data-tb%3Aj_idt202%3Afilter=&aply-data-tb%3Aj_idt204%3Afilter=&aply-data-tb%3Aj_idt206%3Afilter=&aply-data-tb%3Aj_idt208%3Afilter=&aply-data-tb%3Aj_idt210%3Afilter=&aply-data-tb%3Aj_idt215_focus=&aply-data-tb%3Aj_idt215_input=HOUR&aply-data-tb%3Aj_idt219%3Afilter=&aply-data-tb%3Aj_idt222_focus=&aply-data-tb%3Aj_idt222_input=&aply-data-tb%3Aj_idt227_focus=&aply-data-tb%3Aj_idt227_input=&aply-data-tb%3Aj_idt232_focus=&aply-data-tb%3Aj_idt232_input=&aply-data-tb_selection=&aply-data-tb_scrollState=0%2C0&j_idt672%3Adlg-leave-aply-vac-table%3Aj_idt676%3Afilter=&j_idt672%3Adlg-leave-aply-vac-table_selection=&j_idt672%3Adlg-leave-aply-vac-table_scrollState=0%2C0&j_idt729=&j_idt734%3Aj_idt744=&javax.faces.ViewState=${await fetchAnnualLeaveToken()}`.replace(
+        body: `javax.faces.partial.ajax=true&javax.faces.source=search-btn&javax.faces.partial.execute=search-btn+searchContent&javax.faces.partial.render=search-btn+id_tag_toolbar_view+leaveaply-datatable&search-btn=search-btn&formTemplate=formTemplate&j_idt154_input=2022%2F11%2F26&j_idt158_input=2022%2F12%2F25&j_idt166_focus=&j_idt166_input=&j_idt170_focus=&j_idt170_input=&j_idt179_focus=&j_idt179_input=0&aply-data-tb%3Aj_idt193%3Afilter=&aply-data-tb%3Aj_idt195%3Afilter=&aply-data-tb%3Aj_idt197%3Afilter=&aply-data-tb%3Aj_idt199%3Afilter=&aply-data-tb%3Aj_idt202%3Afilter=&aply-data-tb%3Aj_idt204%3Afilter=&aply-data-tb%3Aj_idt206%3Afilter=&aply-data-tb%3Aj_idt208%3Afilter=&aply-data-tb%3Aj_idt210%3Afilter=&aply-data-tb%3Aj_idt215_focus=&aply-data-tb%3Aj_idt215_input=HOUR&aply-data-tb%3Aj_idt219%3Afilter=&aply-data-tb%3Aj_idt222_focus=&aply-data-tb%3Aj_idt222_input=&aply-data-tb%3Aj_idt227_focus=&aply-data-tb%3Aj_idt227_input=&aply-data-tb%3Aj_idt232_focus=&aply-data-tb%3Aj_idt232_input=&aply-data-tb_selection=&aply-data-tb_scrollState=0%2C0&j_idt672%3Adlg-leave-aply-vac-table%3Aj_idt676%3Afilter=&j_idt672%3Adlg-leave-aply-vac-table_selection=&j_idt672%3Adlg-leave-aply-vac-table_scrollState=0%2C0&j_idt729=&j_idt734%3Aj_idt744=&javax.faces.ViewState=${session}`.replace(
             /&j_idt154_input=\d+%2F\d+%2F\d+&j_idt158_input=\d+%2F\d+%2F\d+/,
             searchDateRange
         ),
---
+++ src/werp/interfaces/AjaxPattern.ts
@@ -0,0 +1,9 @@
+interface AjaxPattern {
+    session: string;
+    searchRange: {
+        start: string;
+        end: string;
+    };
+}
+
+export default AjaxPattern;
