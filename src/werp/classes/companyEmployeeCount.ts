00150b43d499575f78d8b3dec7571e3ed604ef48
Kenny <kenny_hung@mail.chungyo.net>
2023-02-21 18:24:10 +0800

    顯示過往的公司在職人數

--- src/werp/classes/companyEmployeeCount.ts
+++ src/werp/classes/companyEmployeeCount.ts
@@ -1,9 +1,18 @@
 import { Chart } from 'chart';
 
 import { getCompanyEmployeeCountObject } from '@/werp/classes/sessionManager';
+import { mergeObject } from '@/common/object';
+
+export const OLD_COMPANY_EMPLOYEE_COUNT: Object = {
+    '2022': { '52': 1497, '53': 1497 },
+    '2023': { '1': 1498, '2': 1496, '3': 1475, '5': 1448, '6': 1401, '7': 1396 },
+};
 
 export const displayCompanyEmployeeCountLineChart = async (): Promise<void> => {
-    const companyEmployeeCountObject: Object = await getCompanyEmployeeCountObject();
+    const companyEmployeeCountObject: Object = mergeObject(
+        await getCompanyEmployeeCountObject(),
+        OLD_COMPANY_EMPLOYEE_COUNT
+    );
     const canvasElement: HTMLCanvasElement = document.getElementById('company_employee_count') as HTMLCanvasElement;
     const data = [];
 
