77a22a402ad939f239e3cea37b62c7aaf511a9c8
Kenny <kenny_hung@mail.chungyo.net>
2023-03-29 13:52:34 +0800

    新增 Line 的環境變數檔案

---
+++ src/werp/consts/env.ts
@@ -0,0 +1,3 @@
+export const LINE_USER_ID: string = GM_getValue('LINE_USER_ID', '');
+export const LINE_MESSAGING_API_ACCESS_TOKEN: string = GM_getValue('LINE_MESSAGING_API_ACCESS_TOKEN', '');
+export const LINE_NOTIFY_CHANNEL_ACCESS_TOKEN: string = GM_getValue('LINE_NOTIFY_CHANNEL_ACCESS_TOKEN', '');
