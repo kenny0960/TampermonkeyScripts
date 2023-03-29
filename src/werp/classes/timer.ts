581f174fa06261e5448901a21ff397938e9cf379
Kenny <kenny_hung@mail.chungyo.net>
2023-03-29 14:07:10 +0800

    分開出勤和通知的更新頻率

--- src/werp/classes/timer.ts
+++ src/werp/classes/timer.ts
@@ -8,28 +8,44 @@ import { log } from '@/common/logger';
 import UPDATE_LOGS from '@/werp/consts/UpdateLogs';
 import { updatePredictedSignOutProgressBar } from '@/werp/classes/progressBar';
 import { updatePredictedSignOutTemplate, updateAttendanceSummary } from '@/werp/classes/attendanceTable';
+import { TIMER_ATTENDANCE_SECONDS, TIMER_NOTIFICATION_MINUTES } from '@/werp/consts/env';
 
 export const resetAttendanceTimers = (): void => {
     window.clearTimeout(Number(SessionManager.getByKey(SessionKeys.ATTENDANCE_TIMER)));
+    window.clearTimeout(Number(SessionManager.getByKey(SessionKeys.NOTIFICATION_TIMER)));
+};
+
+export const startAttendanceTimer = (tableSectionElement: HTMLTableSectionElement, attendances: Attendance[]) => {
+    updatePredictedSignOutTemplate(attendances);
+    updatePredictedSignOutProgressBar(tableSectionElement, attendances);
+    updateAttendanceSummary(attendances);
+    updateAttendanceFavicon(attendances);
+    // 定時更新內容
+    const attendanceTimer: number = window.setTimeout((): void => {
+        log('更新資訊');
+        startAttendanceTimer(tableSectionElement, attendances);
+    }, TIMER_ATTENDANCE_SECONDS * 1000);
+    SessionManager.setByKey(SessionKeys.ATTENDANCE_TIMER, String(attendanceTimer));
+};
+
+export const startNotificationTimer = (attendances: Attendance[]) => {
+    showUpdateLogNotification(UPDATE_LOGS.slice(0, 1)[0]);
+    showAttendanceNotification(attendances);
+    showCompanyNotification();
+
+    // 定時更新內容
+    const notificationTimer: number = window.setTimeout((): void => {
+        log('顯示通知');
+        startNotificationTimer(attendances);
+    }, TIMER_NOTIFICATION_MINUTES * 60 * 1000);
+    SessionManager.setByKey(SessionKeys.NOTIFICATION_TIMER, String(notificationTimer));
 };
 
 export const startAttendanceTimers = (tableSectionElement: HTMLTableSectionElement, attendances: Attendance[]) => {
     for (let i = 1; i < attendances.length; i++) {
         if (isToday(attendances[i].signInDate) === true) {
-            updatePredictedSignOutTemplate(attendances);
-            updatePredictedSignOutProgressBar(tableSectionElement, attendances);
-            updateAttendanceSummary(attendances);
-            updateAttendanceFavicon(attendances);
-            showUpdateLogNotification(UPDATE_LOGS.slice(0, 1)[0]);
-            showAttendanceNotification(attendances);
-            showCompanyNotification();
-
-            // 定時更新內容
-            const attendanceTimer: number = window.setTimeout((): void => {
-                log('更新資訊');
-                startAttendanceTimers(tableSectionElement, attendances);
-            }, 60 * 1000);
-            SessionManager.setByKey(SessionKeys.ATTENDANCE_TIMER, String(attendanceTimer));
+            startAttendanceTimer(tableSectionElement, attendances);
+            startNotificationTimer(attendances);
         }
     }
 };
--- src/werp/consts/env.ts
+++ src/werp/consts/env.ts
@@ -1,3 +1,6 @@
+export const TIMER_ATTENDANCE_SECONDS: number = GM_getValue('TIMER_ATTENDANCE_SECONDS', 60);
+export const TIMER_NOTIFICATION_MINUTES: number = GM_getValue('TIMER_NOTIFICATION_MINUTES', 5);
+
 export const LINE_USER_ID: string = GM_getValue('LINE_USER_ID', '');
 export const LINE_MESSAGING_API_ACCESS_TOKEN: string = GM_getValue('LINE_MESSAGING_API_ACCESS_TOKEN', '');
 export const LINE_NOTIFY_CHANNEL_ACCESS_TOKEN: string = GM_getValue('LINE_NOTIFY_CHANNEL_ACCESS_TOKEN', '');
