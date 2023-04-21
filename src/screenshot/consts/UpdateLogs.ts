afd27e3d920daec5af5f2347f901d5dae7e78430
Kenny <kenny_hung@mail.chungyo.net>
2023-04-21 15:30:33 +0800

    更新 SCREENSHOT V0.0.1

---
+++ src/screenshot/consts/UpdateLogs.ts
@@ -0,0 +1,11 @@
+import UpdateLog from '@/common/interfaces/UpdateLog';
+
+const UPDATE_LOGS: UpdateLog[] = [
+    {
+        version: '0.0.1',
+        date: '20230421 1600',
+        messages: ['實作截圖主畫面功能'],
+    },
+];
+
+export default UPDATE_LOGS;
