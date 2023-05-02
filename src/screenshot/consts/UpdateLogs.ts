b27c88959632f9c13d502eb9c51a924fa81c0f52
Kenny <kenny_hung@mail.chungyo.net>
2023-05-02 15:09:08 +0800

    更新 SCREENSHOT V0.0.2
    - 驗證 selector 的功能

--- src/screenshot/consts/UpdateLogs.ts
+++ src/screenshot/consts/UpdateLogs.ts
@@ -2,6 +2,11 @@ import UpdateLog from '@/common/interfaces/UpdateLog';
 
 const UPDATE_LOGS: UpdateLog[] = [
     {
+        version: '0.0.2',
+        date: '20230502 1530',
+        messages: ['驗證 selector 的功能'],
+    },
+    {
         version: '0.0.1',
         date: '20230421 1600',
         messages: ['實作截圖主畫面功能'],
