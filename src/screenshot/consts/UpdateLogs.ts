70aa5592fe891fff0442714c3968aef98f8049a7
Kenny <kenny_hung@mail.chungyo.net>
2023-05-18 16:35:08 +0800

    更新 SCREENSHOT V0.0.3
    - 實作隨意截傳送當前網址的功能
    - 實作提示傳送訊息的功能

--- src/screenshot/consts/UpdateLogs.ts
+++ src/screenshot/consts/UpdateLogs.ts
@@ -2,6 +2,11 @@ import UpdateLog from '@/common/interfaces/UpdateLog';
 
 const UPDATE_LOGS: UpdateLog[] = [
     {
+        version: '0.0.3',
+        date: '20230518 1630',
+        messages: ['實作隨意截傳送當前網址的功能', '實作提示傳送訊息的功能'],
+    },
+    {
         version: '0.0.2',
         date: '20230502 1530',
         messages: ['驗證 selector 的功能'],
