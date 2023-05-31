5eaf54783f162cbb14a283eb0c2ff47f4fbd2d0f
Kenny <kenny_hung@mail.chungyo.net>
2023-05-31 12:19:33 +0800

    點擊 ESC 鍵就離開隨意截模式

--- src/screenshot/classes/listener.ts
+++ src/screenshot/classes/listener.ts
@@ -1,11 +1,27 @@
 import { sendCanvasElementScreenshot } from '@/common/lineBot/messagingApi';
 import html2canvas from 'html2canvas';
 import { getSelector } from '@/common/element';
+import { closeHighlighting, isHighlighting } from '@/screenshot/classes/element';
 
 export const sendHighlightElement = async (event: MouseEvent): Promise<void> => {
     // 避免直接點擊到鏈結而跳頁
     event.preventDefault();
-    
+    removeHighlightedElement();
+
+    const targetElement: HTMLElement = event.target as HTMLElement;
+    const selector: string = getSelector(targetElement);
+    const canvasElement: HTMLCanvasElement = await html2canvas(targetElement);
+    await sendCanvasElementScreenshot(canvasElement, `${selector} 的截圖`);
+};
+
+export const highlightElement = (event: MouseEvent): void => {
+    removeHighlightedElement();
+
+    const targetElement: HTMLElement = event.target as HTMLElement;
+    targetElement.classList.add('highlight');
+};
+
+export const removeHighlightedElement = (): void => {
     const highlightedElement: HTMLElement | null = document.querySelector('.highlight');
 
     if (highlightedElement === null) {
@@ -13,28 +29,27 @@ export const sendHighlightElement = async (event: MouseEvent): Promise<void> => 
     }
 
     highlightedElement.classList.remove('highlight');
-    const selector: string = getSelector(highlightedElement);
-    const canvasElement: HTMLCanvasElement = await html2canvas(highlightedElement);
-    await sendCanvasElementScreenshot(canvasElement, `${selector} 的截圖`);
 };
 
-export const highlightElement = (event: MouseEvent): void => {
-    const highlightedElement: HTMLElement | null = document.querySelector('.highlight');
-
-    if (highlightedElement !== null) {
-        highlightedElement.classList.remove('highlight');
+export const escapeHighlighting = (event: KeyboardEvent): void => {
+    if (isHighlighting() === false) {
+        return;
     }
-
-    const targetElement: HTMLElement = event.target as HTMLElement;
-    targetElement.classList.add('highlight');
+    if (event.key === 'Escape') {
+        closeHighlighting();
+        removeHighlightedElement();
+        removeHighlightListener();
+    }
 };
 
 export const bindHighlightListener = (): void => {
     document.addEventListener('mousemove', highlightElement);
     document.addEventListener('click', sendHighlightElement);
+    document.addEventListener('keydown', escapeHighlighting);
 };
 
 export const removeHighlightListener = (): void => {
     document.removeEventListener('mousemove', highlightElement);
     document.removeEventListener('click', sendHighlightElement);
+    document.removeEventListener('keydown', escapeHighlighting);
 };
